aa96f336329116c158e83e233a06e5af3167f336
Egbon Of Crypto <112263576+kizzydez@users.noreply.github.com>
2026-07-05 17:49:39 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        googlePurple: '#8e24aa',
+        googleGray: '#5f6368',
+        googleRed: '#d93025',
+      },
+    },
+  },
   plugins: [],
-};
+}

b28aa61850e71aa9c1b54a14159769bca4d54168
Egbon Of Crypto <112263576+kizzydez@users.noreply.github.com>
2026-07-05 17:54:14 +0100

    Simplify Tailwind CSS configuration
    Removed unused theme colors and simplified configuration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        googlePurple: '#8e24aa',
-        googleGray: '#5f6368',
-        googleRed: '#d93025',
-      },
-    },
-  },
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
   plugins: [],
-}
+};
